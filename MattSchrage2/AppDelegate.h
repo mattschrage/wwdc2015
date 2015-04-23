cdbadcaaab27c31e052012571150425c8ba3a1e6
Matt Schrage <matthewschrage@gmail.com>
2015-04-22 18:27:06 -0700

    Initial Commit

---
+++ MattSchrage2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MattSchrage2
+//
+//  Created by Matt Schrage on 4/22/15.
+//  Copyright (c) 2015 Matt Schrage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MattSchrage2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MattSchrage2
+//
+//  Created by Matt Schrage on 4/22/15.
+//  Copyright (c) 2015 Matt Schrage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
